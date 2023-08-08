42d9a33cc9de049c96f902bfd9160d1162e32f34
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-08 16:52:04 -0600

    Add files via upload

---
+++ For_udy/For_udy/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//for (inicializacion; condicion; modificacion de la variable inicializada) { bloque de codigo}
+int contador = 1;
+//contador2 esta solo inicializada dentro del bloque for
+// se puede contar como sea ejemplo contador=contador +2
+for (int contador2=5; contador2<=10; contador2++) {
+    Console.WriteLine(contador2);
+}
